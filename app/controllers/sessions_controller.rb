84b51b1e486e2567daf00845ab7090f0588b3cd6
theonlyjames <james@jamesmk.com>
2013-05-13 19:58:58 -0700

    added user model for individule sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,14 @@ class SessionsController < ApplicationController
 
   def create
     #raise request.env["omniauth.auth"].to_yaml
-    session[:access_token] = request.env['omniauth.auth']['credentials']['token']
-    session[:access_secret] = request.env['omniauth.auth']['credentials']['secret']
-    redirect_to show_path, notice: "Signed in"
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid']).first || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to show_path, :notice => "Signed in!"
+    # session[:access_token] = request.env['omniauth.auth']['credentials']['token']
+    # session[:access_secret] = request.env['omniauth.auth']['credentials']['secret']
+    # redirect_to show_path, notice: "Signed in"
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121010132058) do
+ActiveRecord::Schema.define(:version => 20130514024328) do
 
   create_table "tasks", :force => true do |t|
     t.string   "title"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(:version => 20121010132058) do
     t.string   "status"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
